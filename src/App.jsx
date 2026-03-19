099aee65b10e075183494735a2f87db904bfbf78
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-03-20 04:29:08 +0500

    made few changes to nevbar

--- src/App.jsx
+++ src/App.jsx
@@ -42,12 +42,6 @@ const App = () => {
                   </svg>
                 }>Login</NavLink>
 
-                <NavLink href="/register" icon={
-                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-                  </svg>
-                }>Register</NavLink>
-
                 <NavLink href="/tasks" icon={
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
